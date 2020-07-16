44a4f453cbb8d78c83107eb2e59428bb1c60a791
Jeremy Flowers <flowersje08@gmail.com>
2020-07-15 20:04:31 -0700

    Updates

---
+++ precache-manifest.faadf99e88b01fbaafc93654450ffd1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116d2a7de8def8976fd2726a389a760c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1a87b02b14d7f5057aae",
+    "url": "/react-portfolio/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "d1feda01ee656a542d26",
+    "url": "/react-portfolio/static/js/2.5e32b02b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.5e32b02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a87b02b14d7f5057aae",
+    "url": "/react-portfolio/static/js/main.cf46baf2.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "7eec437de55e64b1a925de4532eb793d",
+    "url": "/react-portfolio/static/media/noatsBanner.7eec437d.png"
+  }
+]);
